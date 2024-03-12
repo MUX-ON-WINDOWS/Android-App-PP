cbd0baed2c0b0bbeee624008eb6b21e04ba6c3c6
MUX_ON_WINDOWS <maxnouts@gmail.com>
2024-03-12 11:15:30 +0100

    Added login with JSON. You can now login with ur user info. fetched from JSON file

--- LoginScreenApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ LoginScreenApp/app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.personalproject
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.example.helloworldapp.Models.User
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.google.gson.Gson
-import java.io.File
-import java.io.InputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,35 +22,42 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun readJSONFromAsset(): String? {
-        var json: String? = null
-        try {
-            val  inputStream:InputStream = assets.open("users.json")
-            json = inputStream.bufferedReader().use{it.readText()}
-            println(json)
-        } catch (ex: Exception) {
-            ex.printStackTrace()
-            return null
-        }
-        return json
-    }
-
-    fun parseJSON() {
-        Gson().fromJson(readJSONFromAsset(), HomepageActivity::class.java)
-    }
-
     fun loginButton(view: View) {
         val username = findViewById<EditText>(R.id.username)
-        val user = username.text.toString()
+        val userField = username.text.toString()
         val password = findViewById<EditText>(R.id.password)
-        val pass = password.text.toString()
+        val passField = password.text.toString()
+        val gson = Gson()
 
-        if (pass == "pass" && user == "user") {
-            Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, HomepageActivity::class.java)
-            startActivity(intent)
-        } else {
-            Toast.makeText(this, "LOGIN FAILED", Toast.LENGTH_SHORT).show()
+        val users:Array<User> = gson.fromJson(ReadJSONFromAssets(baseContext, "users.json")
+            , Array<User>::class.java)
+
+        for (user in users) {
+            if (passField == user.password && userField == user.username) {
+                Toast.makeText(this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, HomepageActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "LOGIN FAILED", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun ReadJSONFromAssets(context: Context, path: String): String {
+        val identifier = "[ReadJSON]"
+        try {
+            val file = context.assets.open("$path")
+            val bufferedReader = BufferedReader(InputStreamReader(file))
+            val stringBuilder = StringBuilder()
+            bufferedReader.useLines { lines ->
+                lines.forEach {
+                    stringBuilder.append(it)
+                }
+            }
+            return stringBuilder.toString()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ""
         }
     }
 
---
+++ LoginScreenApp/app/src/main/java/com/example/helloworldapp/Models/Users.kt
@@ -0,0 +1,7 @@
+package com.example.locationapp.Models
+
+import com.example.helloworldapp.Models.User
+
+data class Users(
+    val users: List<User>
+)
